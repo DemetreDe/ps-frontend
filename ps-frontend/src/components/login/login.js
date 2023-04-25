410387c5edfadc3c5190221a5f6d3fdcce245ff4
Demetre <demetre.de.alwis@gmail.com>
2023-04-25 22:08:51 +0530

    added token handling for login and context

--- ps-frontend/src/components/login/login.js
+++ ps-frontend/src/components/login/login.js
@@ -1,33 +1,45 @@
 import React, { useRef, useContext } from "react";
-import './login.css';
+import "./login.css";
 import AuthContext from "../../context/auth-context";
+import axios from "axios";
 
 let Login = function () {
-    const authCtx = useContext(AuthContext);
+  const authCtx = useContext(AuthContext);
 
-    const passwordRef = useRef();
-    const usernameRef = useRef()
+  const passwordRef = useRef();
+  const usernameRef = useRef();
 
-    //VALIDATION
-    //LOGIN REQUEST
-    //SEND TOKEN TO CONTEXT
-    const handleLogin = (e) => {
-        e.preventDefault();
-        console.log(usernameRef.current.value, passwordRef.current.value);
-        authCtx.onLogin(usernameRef.current.value, passwordRef.current.value);
-    }
+  //VALIDATION
+  //LOGIN REQUEST
+  //SEND TOKEN TO CONTEXT
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log(usernameRef.current.value, passwordRef.current.value);
+    axios
+      .post("http://localhost:8080/api/auth/authenticate", {
+        email: usernameRef.current.value,
+        password: passwordRef.current.value,
+      })
+      .then((response) => {
+        console.log(response.data.token);
+        authCtx.onLogin(response.data.token);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-    return (
-        <div className="auth-form-container">
-            <form className="login-form" onSubmit={handleLogin}>
-                <label htmlFor="username">Username</label>
-                <input type="text" placeholder="Username" ref={usernameRef} />
-                <label htmlFor="password">Password</label>
-                <input type="password" placeholder="Password" ref={passwordRef} />
-                <button type="submit">Log In</button>
-            </form>
-        </div>
-    )
+  return (
+    <div className="auth-form-container">
+      <form className="login-form" onSubmit={handleLogin}>
+        <label htmlFor="username">Username</label>
+        <input type="text" placeholder="Username" ref={usernameRef} />
+        <label htmlFor="password">Password</label>
+        <input type="password" placeholder="Password" ref={passwordRef} />
+        <button type="submit">Log In</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
--- ps-frontend/src/context/auth-context.js
+++ ps-frontend/src/context/auth-context.js
@@ -1,43 +1,49 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-//STORE TOKEN
 const AuthContext = React.createContext({
-    isLoggedIn: false,
-    onLogout: () => { },
-    onLogin: (email, password) => { }
+  isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {},
 });
 
 export const AuthContextProvider = (props) => {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    useEffect(() => {
-        const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
-        if (storedUserLoggedInInformation === '1') {
-            setIsLoggedIn(true);
-        }
-    }, []);
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+    if (storedUserLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
 
-    const logoutHandler = () => {
-        localStorage.removeItem('isLoggedIn');
-        setIsLoggedIn(false);
-    };
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    localStorage.removeItem("token");
+    setIsLoggedIn(false);
+  };
 
-    const loginHandler = () => {
-        localStorage.setItem('isLoggedIn', '1');
-        setIsLoggedIn(true);
-    };
+  const loginHandler = (token) => {
+    if (token === undefined || token === null) return;
+    localStorage.setItem("isLoggedIn", "1");
+    localStorage.setItem("token", token);
+    setIsLoggedIn(true);
+  };
 
-    return (
-        <AuthContext.Provider
-            value={{
-                isLoggedIn: isLoggedIn,
-                onLogout: logoutHandler,
-                onLogin: loginHandler,
-            }}
-        >
-            {props.children}
-        </AuthContext.Provider>
-    );
+  const getToken = () => {
+    return localStorage.getItem("token");
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthContext;
